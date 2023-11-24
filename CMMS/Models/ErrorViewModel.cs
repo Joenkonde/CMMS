24acd4707dda830dbe9ba264c4e43c81ec716ef8
Dell <Dell@DESKTOP-IKR2NHN>
2023-11-24 23:10:48 +0300

    Add project files.

---
+++ CMMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
